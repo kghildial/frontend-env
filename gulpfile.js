8b8b8aaf2dc67df2d5df46b67105a181b55cead4
Kinshuk Ghildial <dev.kghildial@gmail.com>
2017-12-27 17:58:00 +0530

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+
+// Compile sass
+gulp.task('sass', function() {
+  return gulp.src(['src/scss/*.scss'])
+  .pipe(sass())
+  .pipe(gulp.dest('src/css'))
+  .pipe(browserSync.stream());
+});
+
+//Watch & serve
+gulp.task('serve', ['sass'], function() {
+  browserSync.init({
+    server: './src'
+  });
+
+  gulp.watch(['src/scss/*.scss'], ['sass']);
+  gulp.watch(['src/*.html']).on('change', browserSync.reload);
+});
+
+//Default
+gulp.task('default', ['serve']);
